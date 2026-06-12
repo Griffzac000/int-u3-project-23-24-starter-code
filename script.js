45d5ab5dbaff79e0c660144cf2b53723d9432ade
griffzac000 <griffzac000@warren.k12.in.us>
2026-06-12 13:36:14 -0400

    Initial

--- script.js
+++ script.js
@@ -5,11 +5,30 @@
 
 
 
-
-
-
 // When you're ready to make event handlers, uncomment the code below. 
 //  - Then fill in the blanks with the correct variables.
+let optionOneBtn = document.querySelector('.option-one');
+let optionOneScreen = document.querySelector('.option-one-screen');
+let opening = document.querySelector('.story-opening')
+let optionTwoBtn = document.querySelector('.option-two')
+let optionTwoScreen = document.querySelector('.option-two-screen')
+let body = document.querySelector('body')
+let userBtn = document.querySelector('.user')
+
+
+userBtn.addEventListener('click', function(){
+     
+})
+optionOneBtn.addEventListener('click', function(){
+    optionOneScreen.style.display = 'block';
+    opening.style.display = 'none';
+    body.style.backgroundImage = "url('')"
+});
+optionTwoBtn.addEventListener('click', function(){
+    optionTwoScreen.style.display = 'block';
+    opening.style.display = 'none';
+    body.style.backgroundImage = "url('')"
+});
 
 
 // INSERT_VARIABLE.addEventListener('click', function(){
