4eb9f2f4b7e0eb93b63351822ded9be75227bf5a
griffzac000 <griffzac000@warren.k12.in.us>
2026-06-13 00:45:06 -0400

    init

--- script.js
+++ script.js
@@ -1,45 +1,36 @@
-// Declare variables below to save the different divs of your story.
 
-
-
-
-
-
-// When you're ready to make event handlers, uncomment the code below. 
-//  - Then fill in the blanks with the correct variables.
 let optionOneBtn = document.querySelector('.option-one');
 let optionOneScreen = document.querySelector('.option-one-screen');
-let opening = document.querySelector('.story-opening')
+let optionOneEnd = document.querySelector('.option-one-end')
+let opening = document.querySelector('.opening')
 let optionTwoBtn = document.querySelector('.option-two')
 let optionTwoScreen = document.querySelector('.option-two-screen')
+let optionTwoEnd = document.querySelector('.option-two-end')
 let body = document.querySelector('body')
-let userBtn = document.querySelector('.user')
+let userBtn = document.querySelector('#user');
+let userName = document.querySelector('#userName')
+const inputElement = document.getElementById('myUsername')
 
 
 userBtn.addEventListener('click', function(){
-     
-})
-optionOneBtn.addEventListener('click', function(){
+    const userData = inputElement.value;
+    console.log(userData);
     optionOneScreen.style.display = 'block';
     opening.style.display = 'none';
-    body.style.backgroundImage = "url('')"
+    body.style.backgroundImage = "url('https://i.pinimg.com/originals/1a/7a/3c/1a7a3c32c2fd248ca4ac3b043e4cf786.gif')";
+    document.getElementById('userName').textContent = userData;
 });
+optionOneBtn.addEventListener('click', function(){
+    console.log("clicked");
+    optionTwoScreen.style.display = 'none';
+    optionOneScreen.style.display = 'block';
+    body.style.backgroundImage = "url('https://www.worldanvil.com/uploads/images/91ef3b86a30d831e4a0409bdb1280af0.gif')";
+});
+
+
+
 optionTwoBtn.addEventListener('click', function(){
     optionTwoScreen.style.display = 'block';
-    opening.style.display = 'none';
-    body.style.backgroundImage = "url('')"
+    optionOneScreen.style.display = 'none';
+    body.style.backgroundImage = "url('https://www.worldanvil.com/uploads/images/91ef3b86a30d831e4a0409bdb1280af0.gif')";
 });
-
-
-// INSERT_VARIABLE.addEventListener('click', function(){
-
-// });
-
-// INSERT_VARIABLE.addEventListener('click', function(){
-
-// });
-
-
-// INSERT_VARIABLE.addEventListener('click', function(){
-
-// });
